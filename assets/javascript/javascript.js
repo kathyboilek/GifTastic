d489abb645a28d286bf93c4284209bd55c76e459
Kathleen Boilek <kaboilek@gmail.com>
2018-06-26 17:52:46 -0400

    added comments to javascript

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,10 +1,14 @@
 $( document ).ready(function() {
-	// An array of topics, new topics will be pushed into this array;
-    var topics = ["film noir", "musicals","betty davis", "cary grant", "frank sinatra", "alfred hitchcock"];
-    // Creating Functions & Methods
+	// Create an array of strings and save it to a variable called `topics`.
+	var topics = ["film noir", "musicals","betty davis", "cary grant", "frank sinatra", "alfred hitchcock", "Snoopy"];
+	
+	// Creating Functions & Methods
+	
 	// Function that displays all gif buttons
 	function displayGifButtons(){
-		$("#gifButtonsView").empty(); // erasing anything in this div id so that it doesnt duplicate the results
+
+		// erasing anything in this div id so that it doesnt duplicate the results
+		$("#gifButtonsView").empty(); 
 		for (var i = 0; i < topics.length; i++){
 			var gifButton = $("<button>");
 			gifButton.addClass("topic");
@@ -14,25 +18,17 @@ $( document ).ready(function() {
 			$("#gifButtonsView").append(gifButton);
 		}
 	}
-	// Function to add a new topic button
+	// Function to add a new topic button by user
 	function addNewButton(){
 		$("#addGif").on("click", function(){
 		var topic = $("#topic-input").val().trim();
 		if (topic == ""){
-		  return false; // added so user cannot add a blank button
+
+		// added so user cannot add a blank button
+		  return false; 
 		}
 		topics.push(topic);
 	
-		displayGifButtons();
-		return false;
-		});
-	}
-	// Function to remove last topic button
-		// Doesnt work properly yet removes all of the added buttons
-		// rather than just the last
-	function removeLastButton(){
-		$("removeGif").on("click", function(){
-		topics.pop();
 		displayGifButtons();
 		return false;
 		});
@@ -41,43 +37,65 @@ $( document ).ready(function() {
 	function displayGifs(){
 		var topic = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=2nL14eXC6EbDFnaTzU5cPer5r7GaxUCl&limit=10";		
-        console.log(queryURL); // displays the constructed url
+
+		// displays the constructed url
+		console.log(queryURL); 
 		$.ajax({
 			url: queryURL,
 			method: 'GET'
 		})
 		.done(function(response) {
-			console.log(response); // console test to make sure something returns
-			$("#gifsView").empty(); // erasing anything in this div id so that it doesnt keep any from the previous click
-			var results = response.data; //shows results of gifs
+
+			// console test to make sure something returns
+			console.log(response); 
+
+			// erasing anything in this div id so that it doesnt keep any from the previous click
+			$("#gifsView").empty(); 
+
+			//shows results of gifs
+			var results = response.data; 
 			if (results == ""){
 			  alert("There isn't a gif for this selected button");
 			}
 			for (var i=0; i<results.length; i++){
 	
-				var gifDiv = $("<div>"); //div for the gifs to go inside
+				//div for the gifs to go inside
+				var gifDiv = $("<div>"); 
 				gifDiv.addClass("gifDiv");
 				// pulling rating of gif
 				var gifRating = $("<p>").text("Rating: " + results[i].rating);
 				gifDiv.append(gifRating);
 				// pulling gif
 				var gifImage = $("<img>");
-				gifImage.attr("src", results[i].images.fixed_height_small_still.url); // still image stored into src of image
-				gifImage.attr("data-still",results[i].images.fixed_height_small_still.url); // still image
-				gifImage.attr("data-animate",results[i].images.fixed_height_small.url); // animated image
-				gifImage.attr("data-state", "still"); // set the image state
+
+				// still image stored into src of image
+				gifImage.attr("src", results[i].images.fixed_height_small_still.url); 
+
+				// still image stored into src of image
+				gifImage.attr("data-still",results[i].images.fixed_height_small_still.url);
+
+				// animated image stored into src of image
+				gifImage.attr("data-animate",results[i].images.fixed_height_small.url); 
+
+				// set the image state stored into src of image
+				gifImage.attr("data-state", "still"); 
 				gifImage.addClass("image");
-				gifDiv.append(gifImage);
+
 				// pulling still image of gif
+				gifDiv.append(gifImage);
+
+
 				// adding div of gifs to gifsView div
 				$("#gifsView").prepend(gifDiv);
 			}
 		});
 	}
 	// Calling Functions & Methods
-	displayGifButtons(); // displays list of topics already created
+
+	// displays list of topics already created	
+	displayGifButtons(); 
 	addNewButton();
-	removeLastButton();
+
 	// Document Event Listeners
 	$(document).on("click", ".topic", displayGifs);
 	$(document).on("click", ".image", function(){
